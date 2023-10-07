e494eedbbe9be5cdf7cd6498c7764a538d7aace7
HanRachid <handaouirachid@gmail.com>
2023-10-07 19:59:06 +0100

    please cooperate vercel

--- backend/index.js
+++ backend/index.js
@@ -53,3 +53,25 @@ app.use(options);
 app.listen(port, () => {
   console.log('listening on port ' + port);
 });
+
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+app.use(allowCors);
