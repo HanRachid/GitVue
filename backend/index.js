3a47a6a577d840e12da917310e53f24b678a5ca8
HanRachid <handaouirachid@gmail.com>
2023-10-07 19:49:10 +0100

    please cooperate vercel

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,9 @@ import express from 'express';
 import {getAccessCode, getSession, getSessionCodeUrl} from './api/repositories';
 
 const app = express();
-var cors = require('cors');
 const port = process.env.VITE_CLIENT_PORT | 3000;
 const clientId = process.env.VITE_CLIENT_ID;
 const clientSecret = process.env.VITE_CLIENT_SECRET;
-app.use(cors());
 
 //redirect user to OAuth window
 app.get('/Oauth', (req, res) => {
