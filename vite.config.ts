2156d0e0422efab2f5ad429a142eb0837af9afb4
HanRachid <handaouirachid@gmail.com>
2023-10-10 10:01:22 +0100

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/rachid_handaoui-taas-frontend-challenge',
+	base: './rachid_handaoui-taas-frontend-challenge',
 	plugins: [vue()],
 });
